2d3a67f5d8ae02aaf0f7c5cb6869263bb212471a
yusalie <yusuf.salie96@gmail.com>
2021-06-09 15:26:52 +0200

    09 Jun 2021

---
+++ tests/integration/test_models.py
@@ -0,0 +1,45 @@
+from tests.test_base import BaseTest
+from market.__init__ import db
+from market.models import User
+from flask_login import current_user
+from flask import request
+from market.models import User, Item
+
+class somethingModel(BaseTest):
+    def test_user_crud(self):
+        with self.app_context():
+            user = User(username='something', email_address='somethings@gmail.com', password_hash='123456')
+            result = db.session.query(User).filter_by(username="something").first()
+            self.assertIsNone(result)
+            
+            db.session.add(user)
+            db.session.commit()
+            
+            result = db.session.query(User).filter_by(username="something").first()
+            self.assertIsNotNone(result)
+            # assert user in db.session
+            
+            db.session.delete(user)
+            db.session.commit()
+            
+            result = db.session.query(User).filter_by(username="something").first()
+            self.assertIsNone(result)
+    
+    def test_item_crud(self):
+        with self.app_context():
+            item = Item(name='RTX 3080', price=1200, barcode='8555632047963', description='description')
+            result = db.session.query(Item).filter_by(name="RTX 3080").first()
+            self.assertIsNone(result)
+            
+            db.session.add(item)
+            db.session.commit()
+            
+            result = db.session.query(Item).filter_by(name="RTX 3080").first()
+            self.assertIsNotNone(result)
+            # assert user in db.session
+            
+            db.session.delete(item)
+            db.session.commit()
+            
+            result = db.session.query(Item).filter_by(name="RTX 3080").first()
+            self.assertIsNone(result)
---
+++ tests/unit/test_route.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+from market import app
+
+class TestRoute(TestCase):
+    def test_home(self):
+        with app.test_client() as client:
+            response = client.get('/home')
+            
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'/home', response.get_data())
+    
+    def test_register(self):
+        with app.test_client() as client:
+            response = client.get('/register')
+            
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'/register', response.get_data())
+    
+    def test_login(self):
+        with app.test_client() as client:
+            response = client.get('/login')
+            
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'/login', response.get_data())
+            
+            
+    def test_logout(self):
+        with app.test_client() as client:
+            response = client.get('/logout')
+            self.assertEqual(response.status_code, 302)
